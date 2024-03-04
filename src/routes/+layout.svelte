a85d4e0dcebdfa4a54eb88c46cf6fe696eefa51f
Ayoub <ayoub.fiadh@gmail.com>
2024-03-03 20:53:32 -0500

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import "../app.css";
+</script>
+
+<div class="app">
+	<main>
+		<slot />
+	</main>
+
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+</style>
